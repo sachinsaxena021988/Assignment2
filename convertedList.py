7a6ab10b19c0600016df88ff1b0b584c5e94518a
sachinsaxena021988 <43206328+sachinsaxena021988@users.noreply.github.com>
2018-09-14 08:06:40 +0530

    comma-separated numbers
    1. Write a program which accepts a sequence of comma-separated numbers from console and
    generate a list.

---
+++ convertedList.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+# please provide the user input
+userinput =input("please provide the number comma seprated :" )
+# convert the string to list
+number_list =userinput.split(',')
+#print the final list
+print(number_list)
+

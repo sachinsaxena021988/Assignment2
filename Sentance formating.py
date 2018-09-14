2532ff65027e712c19c648b651c92c6477eec06e
sachinsaxena021988 <43206328+sachinsaxena021988@users.noreply.github.com>
2018-09-14 08:11:51 +0530

    Sentence formating
    Write a Python Program to print the given string in the format specified in the sample
    output.
    WE, THE PEOPLE OF INDIA, having solemnly resolved to constitute India into a SOVEREIGN,
    SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC and to secure to all its citizens

---
+++ Sentance formating.py
@@ -0,0 +1,9 @@
+
+# coding: utf-8
+
+# In[33]:
+
+
+print("""WE, THE PEOPLE OF INDIA,\n\t having solemnly resolved to constitute India into a SOVEREIGN,!
+ \t\t SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC \n\t\t\v and to secure to all its citizens""")
+
